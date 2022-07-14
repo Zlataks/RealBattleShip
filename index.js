029c8416d98ae86534365af4a96d7984dc137a29
Kseniia Briaschei <briaschei@gmail.com>
2022-07-14 11:03:03 +0300

    realize display object

---
+++ index.js
@@ -0,0 +1,18 @@
+let display = {
+    displayMessage: function (msg) {
+        let messageArea = document.getElementById('messageArea');
+        messageArea.innerHTML = msg;
+    },
+    displayMiss: function (location) {
+        let ship = document.getElementById(location);
+        ship.setAttribute('class', 'miss');
+    },
+    displayHit: function (location) {
+        let ship = document.getElementById(location);
+        ship.setAttribute('class', 'hit');
+    }
+};
+
+/* let guessInput = document.getElementById('guessInput');
+let fireButton = document.getElementById('fireButton');
+fireButton.addEventListener('click', display.displayMessage('new message')); */
